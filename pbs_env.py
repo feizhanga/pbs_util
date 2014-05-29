ca947cd001c2a5ed126e90ba53af15c262529c7a
Fei Zhang <fei.zhang@ga.gov.au>
2014-05-29 21:22:19 +1000

    first commit

---
+++ pbs_env.py
@@ -0,0 +1,13 @@
+"""
+pbs running job environment variables including $PBS_JOBID $PBS_JOBNAME, $PBS_JOBFS  $.....
+"""
+__author__ = 'fzhang'
+
+class PBS_ENV:
+    def __init__(self):
+        return
+
+    def initialize(self):
+        """
+        initialize the environmental variables
+        """
